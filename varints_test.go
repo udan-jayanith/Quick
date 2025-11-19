a584efdffa77bd7099cf6dfcabde15f4ed34d71e
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-11-19 22:38:25 +0530

    Add more test cases for TestVarInt

--- varints_test.go
+++ varints_test.go
@@ -7,15 +7,21 @@ import (
 )
 
 func TestVarInt(t *testing.T) {
-	b, err := quick.ToVarint(60)
-	if err != nil {
-		t.Fatal(err)
+	for _, num := range [...]int64{0, 60, 63, 16383, 1073741823, 4611686018427387903, 4611686018427387903 / 2} {
+		b, err := quick.ToVarint(num)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		v, err := quick.VarintToInt64(b)
+		if err != nil {
+			t.Fatal(err)
+		} else if v != num {
+			t.Fatal("Expected", num, "but got", v)
+		}
 	}
 
-	v, err := quick.VarintToInt64(b)
-	if err != nil {
-		t.Fatal(err)
-	} else if v != 60 {
-		t.Fatal("Expected 60 but got", v)
+	if _, err := quick.ToVarint(4611686018427387903 + 1); err == nil {
+		t.Fatal("Expected a error but got nil")
 	}
 }
