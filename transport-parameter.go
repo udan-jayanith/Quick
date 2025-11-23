3adfc328dc30bd8c587c80c88ce457fed6e354da
Udan Jayanith <j.udanjayanith@gmail.com>
2025-11-23 13:17:38 +0530

    Add TransportParameter, TransportParameterID and transport parameter id values

--- transport-parameter.go
+++ transport-parameter.go
@@ -1,1 +1,130 @@
 package Quick
+
+import (
+	"github.com/udan-jayanith/Quick/varint"
+)
+
+/*
+Transport Parameter {
+  Transport Parameter ID (i),
+  Transport Parameter Length (i),
+  Transport Parameter Value (..),
+}
+*/
+
+// https://datatracker.ietf.org/doc/html/rfc9000#name-transport-parameter-definit
+type TransportParameterID = varint.Int62
+
+type TransportParameter struct {
+	ID     TransportParameterID
+	Length varint.Int62
+	Value  []byte
+}
+
+const (
+	/*
+		(0x00):
+		This parameter is the value of the Destination Connection ID field from the first Initial packet sent by the client; see Section 7.3. This transport parameter is only sent by a server.
+	*/
+	Original_destination_connection_id TransportParameterID = 0x00 + iota
+
+	/*
+		(0x01):
+		The maximum idle timeout is a value in milliseconds that is encoded as an integer; see (Section 10.1). Idle timeout is disabled when both endpoints omit this transport parameter or specify a value of 0.
+	*/
+	Max_idle_timeout
+	/*
+		(0x02):
+		A stateless reset token is used in verifying a stateless reset; see Section 10.3. This parameter is a sequence of 16 bytes. This transport parameter MUST NOT be sent by a client but MAY be sent by a server. A server that does not send this transport parameter cannot use stateless reset (Section 10.3) for the connection ID negotiated during the handshake.
+	*/
+	Stateless_reset_token
+
+	/*
+		(0x03):
+		The maximum UDP payload size parameter is an integer value that limits the size of UDP payloads that the endpoint is willing to receive. UDP datagrams with payloads larger than this limit are not likely to be processed by the receiver.
+
+		The default for this parameter is the maximum permitted UDP payload of 65527. Values below 1200 are invalid.
+
+		This limit does act as an additional constraint on datagram size in the same way as the path MTU, but it is a property of the endpoint and not the path; see Section 14. It is expected that this is the space an endpoint dedicates to holding incoming packets.
+	*/
+	Max_udp_payload_size
+
+	/*
+		(0x04):
+		The initial maximum data parameter is an integer value that contains the initial value for the maximum amount of data that can be sent on the connection. This is equivalent to sending a MAX_DATA (Section 19.9) for the connection immediately after completing the handshake.
+	*/
+	Initial_max_data
+
+	/*
+		(0x05):
+		This parameter is an integer value specifying the initial flow control limit for locally initiated bidirectional streams. This limit applies to newly created bidirectional streams opened by the endpoint that sends the transport parameter. In client transport parameters, this applies to streams with an identifier with the least significant two bits set to 0x00; in server transport parameters, this applies to streams with the least significant two bits set to 0x01.
+	*/
+	Initial_max_stream_data_bidi_local
+
+	/*
+		(0x06):
+		This parameter is an integer value specifying the initial flow control limit for peer-initiated bidirectional streams. This limit applies to newly created bidirectional streams opened by the endpoint that receives the transport parameter. In client transport parameters, this applies to streams with an identifier with the least significant two bits set to 0x01; in server transport parameters, this applies to streams with the least significant two bits set to 0x00.
+	*/
+	Initial_max_stream_data_bidi_remote
+
+	/*
+		(0x07):
+		This parameter is an integer value specifying the initial flow control limit for unidirectional streams. This limit applies to newly created unidirectional streams opened by the endpoint that receives the transport parameter. In client transport parameters, this applies to streams with an identifier with the least significant two bits set to 0x03; in server transport parameters, this applies to streams with the least significant two bits set to 0x02.
+	*/
+	Initial_max_stream_data_uni
+
+	/*
+		(0x08):
+		The initial maximum bidirectional streams parameter is an integer value that contains the initial maximum number of bidirectional streams the endpoint that receives this transport parameter is permitted to initiate. If this parameter is absent or zero, the peer cannot open bidirectional streams until a MAX_STREAMS frame is sent. Setting this parameter is equivalent to sending a MAX_STREAMS (Section 19.11) of the corresponding type with the same value.
+	*/
+	Initial_max_streams_bidi
+
+	/*
+		(0x09):
+		The initial maximum unidirectional streams parameter is an integer value that contains the initial maximum number of unidirectional streams the endpoint that receives this transport parameter is permitted to initiate. If this parameter is absent or zero, the peer cannot open unidirectional streams until a MAX_STREAMS frame is sent. Setting this parameter is equivalent to sending a MAX_STREAMS (Section 19.11) of the corresponding type with the same value.
+	*/
+	Initial_max_streams_uni
+
+	/*
+		(0x0a):
+		The acknowledgment delay exponent is an integer value indicating an exponent used to decode the ACK Delay field in the ACK frame (Section 19.3). If this value is absent, a default value of 3 is assumed (indicating a multiplier of 8). Values above 20 are invalid.
+	*/
+	Ack_delay_exponent
+
+	/*
+		(0x0b):
+		The maximum acknowledgment delay is an integer value indicating the maximum amount of time in milliseconds by which the endpoint will delay sending acknowledgments. This value SHOULD include the receiver's expected delays in alarms firing. For example, if a receiver sets a timer for 5ms and alarms commonly fire up to 1ms late, then it should send a max_ack_delay of 6ms. If this value is absent, a default of 25 milliseconds is assumed. Values of 214 or greater are invalid.
+	*/
+	Max_ack_delay
+
+	/*
+		(0x0c):
+		The disable active migration transport parameter is included if the endpoint does not support active connection migration (Section 9) on the address being used during the handshake. An endpoint that receives this transport parameter MUST NOT use a new local address when sending to the address that the peer used during the handshake. This transport parameter does not prohibit connection migration after a client has acted on a preferred_address transport parameter. This parameter is a zero-length value.
+	*/
+	Disable_active_migration
+
+	/*
+		(0x0d):
+		The server's preferred address is used to effect a change in server address at the end of the handshake, as described in Section 9.6. This transport parameter is only sent by a server. Servers MAY choose to only send a preferred address of one address family by sending an all-zero address and port (0.0.0.0:0 or [::]:0) for the other family. IP addresses are encoded in network byte order.
+		https://datatracker.ietf.org/doc/html/rfc9000#section-18.2-4.38.1
+	*/
+	Preferred_address
+
+	/*
+		(0x0e):
+		This is an integer value specifying the maximum number of connection IDs from the peer that an endpoint is willing to store. This value includes the connection ID received during the handshake, that received in the preferred_address transport parameter, and those received in NEW_CONNECTION_ID frames. The value of the active_connection_id_limit parameter MUST be at least 2. An endpoint that receives a value less than 2 MUST close the connection with an error of type TRANSPORT_PARAMETER_ERROR. If this transport parameter is absent, a default of 2 is assumed. If an endpoint issues a zero-length connection ID, it will never send a NEW_CONNECTION_ID frame and therefore ignores the active_connection_id_limit value received from its peer.
+	*/
+	Active_connection_id_limit
+
+	/*
+		(0x0f):
+		This is the value that the endpoint included in the Source Connection ID field of the first Initial packet it sends for the connection; see Section 7.3.
+	*/
+	Initial_source_connection_id
+
+	/*
+		(0x10):
+		This is the value that the server included in the Source Connection ID field of a Retry packet; see Section 7.3. This transport parameter is only sent by a server.
+	*/
+	Retry_source_connection_id
+)
